9aad86c6dc19c93ca4f993da856b1ac52148cc9c
AyomiCoder <alukostephen01@gmail.com>
2025-03-04 10:36:25 +0100

    test git auth

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -3,8 +3,8 @@ import '../../App.css';
 
 // Define the props interface
 interface NotificationProps {
-    message: string; // Type for message
-    type: 'success' | 'error'; // Type for type; restricts to specific values
+    message: string;
+    type: 'success' | 'error';
 }
 
 const Notification: React.FC<NotificationProps> = ({ message, type }) => {
